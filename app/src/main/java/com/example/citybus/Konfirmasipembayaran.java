7dd0b2a5c58bf03dd3b59dfab0e81ddd3dfd7d6f
nursyahranirani <nursyahranirani05@gmail.com>
2019-08-03 22:23:24 +0800

    ui tinggal 1 layout

--- app/src/main/java/com/example/citybus/Konfirmasipembayaran.java
+++ app/src/main/java/com/example/citybus/Konfirmasipembayaran.java
@@ -2,13 +2,27 @@ package com.example.citybus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Konfirmasipembayaran extends AppCompatActivity {
+    private Button simpan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_konfirmasipembayaran);
+        simpan = (Button)findViewById(R.id.simpan);
+        simpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Konfirmasipembayaran.this, Tampilkantiket.class);
+                startActivity(i);
+            }
+        });
+
     }
+
 }
---
+++ app/src/main/java/com/example/citybus/Tampilkantiket.java
@@ -0,0 +1,14 @@
+package com.example.citybus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tampilkantiket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampilkantiket);
+    }
+}
